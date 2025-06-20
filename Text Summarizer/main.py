885da7771728a6008df4a4ee8cf5db21398180b6
Mohit Mundria <mundriamohit100@gmail.com>
2025-06-20 22:06:18 +0530

    Add all the projects of Digital Blinc Intern till now and update their readme files separetly

--- Text Summarizer/main.py
+++ Text Summarizer/main.py
@@ -5,13 +5,13 @@ import streamlit as st
 
 
 
-def get_response():
+def get_response(User_input):
     
     response = requests.post(
          'http://localhost:11434/api/generate',
          json={
             'model': 'llama3.2:latest',
-            'prompt': f"You are a Text Summarization tool. Make a summary of {User_input} in minimum 50 words and maximum 200 word.",
+            'prompt': f"You are a Text Summarization tool. Make a summary of {User_input} in maximum 6 points.",
             'stream': False
         }
     )
@@ -21,9 +21,9 @@ def get_response():
 st.set_page_config(page_title="Turn long reads into lightning-fast insights — your smart text shrinker is here!", page_icon="📝⚡",layout="centered",initial_sidebar_state='collapsed')
 st.header('Text Summarisation Tool')
 
-User_input=st.text_input("Enter your text here: ",height=200)
+User_input=st.text_input("Enter your text here: ")
 submit=st.button("Summary")
 
 if submit and User_input.strip():
     with st.spinner("Processing..."):
-        st.write(get_response())
+        st.write(get_response(User_input))
